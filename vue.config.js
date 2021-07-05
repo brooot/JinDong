e478e14c660378af6535e2320bab8bd6a50087b3
brooot <1442704671@qq.com>
2021-07-05 11:17:53 +0800

    添加.history到.gitignore中; 添加 vue.config.js 使得打包后的访问路径为相对地址

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Description: 文件描述
+ * @Author: 小火龙
+ * @Date: 2021-07-05 11:07:34
+ * @LastEditTime: 2021-07-05 11:11:44
+ * @FilePath: \JinDong\vue.config.js
+ */
+module.exports = {
+  publicPath: "./",
+  outputDir: "dist",
+  assetsDir: "assets",
+  indexPath: "index.html"
+}
